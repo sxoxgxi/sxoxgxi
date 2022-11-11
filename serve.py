9a8777485fe3f36ee0d9fd2ac31459da86dd512a
SOGI <82437025+sxoxgxi@users.noreply.github.com>
2022-11-11 20:04:25 +0545

    GitHub not rendering Weserv.nl image masking. 😞

--- serve.py
+++ serve.py
@@ -5,25 +5,17 @@ data = res.json()
 quote = data[0]['q']
 author = data[0]['a']
 
-
 def linkify(query: str) -> str:
     return f"https://duckduckgo.com/?q={query.replace(' ', '+')}"
 
-
 with open('README.md', 'r', encoding='utf-8') as readme:
     uwu = readme.read()
-
-# print("-------------------------------------------------------OLD README---------------------------------------------------------")
-# print(uwu, end='\n\n')
 
 startblock = uwu.index("<h4 align='center'>")
 endblock = uwu.index("</a>.</h4>")
 content = f"<h4 align='center'>{quote} - <a href='{linkify(author)}' target='_blank'>{author}</a>.</h4>"
 
 newcontent = uwu[:startblock] + content + uwu[endblock+len("</a>.</h4>"):]
-
-# print("-------------------------------------------------------NEW README---------------------------------------------------------")
-# print(newcontent)
 
 with open("README.md", 'w', encoding='utf-8') as newreadme:
     newreadme.write(newcontent)
