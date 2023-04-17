61f6b603d3077b2636a1c996a8cc7f430aca1b0c
SOGI <sxoxgxi@duck.com>
2023-04-17 17:12:34 +0545

    Reduce redundancy.

--- serve.py
+++ serve.py
@@ -1,23 +1,33 @@
 import requests
 import datetime
-res = requests.get('https://zenquotes.io/api/quotes')
-data = res.json()
-quote = data[0]['q']
-author = data[0]['a']
+
+url = 'https://zenquotes.io/api/quotes'
+response = requests.get(url)
+
+try:
+    data = response.json()
+    quote = data[0]['q']
+    author = data[0]['a']
+except (requests.exceptions.RequestException, ValueError, KeyError) as uwu:
+    quote = "Error while fetching quote."
+    author = f"{uwu}"
+
 
 def linkify(query: str) -> str:
     return f"https://duckduckgo.com/?q={query.replace(' ', '+')}"
 
+
 with open('README.md', 'r', encoding='utf-8') as readme:
-    uwu = readme.read()
+    content = readme.read()
 
-startblock = uwu.index("<h4 align='center'>")
-endblock = uwu.index("</a>.</h4>")
-content = f"<h4 align='center'>{quote} - <a href='{linkify(author)}' target='_blank'>{author}</a>.</h4>"
+startblock = content.index("<h4 align='center'>")
+endblock = content.index("</a>.</h4>")
+new_content = f"<h4 align='center'>{quote} - <a href='{linkify(author)}' target='_blank'>{author}</a>.</h4>"
 
-newcontent = uwu[:startblock] + content + uwu[endblock+len("</a>.</h4>"):]
+new_content = content[:startblock] + new_content + \
+    content[endblock+len("</a>.</h4>"):]
 
-with open("README.md", 'w', encoding='utf-8') as newreadme:
-    newreadme.write(newcontent)
-with open("log.txt", 'a', encoding='utf-8') as log:
+with open("README.md", 'w', encoding='utf-8') as newreadme, \
+        open("log.txt", 'a', encoding='utf-8') as log:
+    newreadme.write(new_content)
     log.write(f"{quote} - {datetime.datetime.now()}\n")
